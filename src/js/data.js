8acc8f6a28979b45bc2cf0057d87272b601ff0ae
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-10-03 04:55:40 +0300

    Add files via upload

---
+++ src/js/data.js
@@ -0,0 +1,298 @@
+export default {
+  airport: {
+    code: `DME`,
+    name: `Домодедово`
+  },
+  arrivals: [
+    {
+      time: `01:00`,
+      destination: `АНАПА`,
+      id: `S7 1128`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `01:00`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `S7 26`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `02:08`,
+      destination: `ДАЛАМАН`,
+      id: `PC 6790`,
+      status: `Прибыл`
+    },
+    {
+      time: `01:40`,
+      destination: `Сочи`,
+      id: `U6 378`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `01:49`,
+      destination: `ЦЮРИХ`,
+      id: `LX 1324`,
+      status: `Прибыл`
+    },
+    {
+      time: `02:42`,
+      destination: `ПУНТА-КАНА`,
+      id: `ZF 978`,
+      status: `Прибыл`
+    },
+    {
+      time: `02:54`,
+      destination: `СОЧИ`,
+      id: `S7 1030`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `02:12`,
+      destination: `CИМФЕРОПОЛЬ`,
+      id: `U6 44`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `03:22`,
+      destination: `БОДРУМ`,
+      id: `PC 6782`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `02:06`,
+      destination: `ИЗМИР`,
+      id: `PC 6778`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `02:13`,
+      destination: `АМСТЕРДАМ`,
+      id: `RU 210`,
+      status: `Прибыл`
+    },
+    {
+      time: `02:56`,
+      destination: `АФИНЫ`,
+      id: `A3 882`,
+      status: `Выдача багажа`
+    },
+    {
+      time: `02:42`,
+      destination: `КИШИНЕВ`,
+      id: `S7 156`,
+      status: `Задержан`
+    },
+    {
+      time: `04:20`,
+      destination: `Лондон`,
+      id: `BA 237`,
+      status: `Задержан`
+    },
+    {
+      time: `05:40`,
+      destination: `ОШ`,
+      id: `SZ 201`,
+      status: `Задержан`
+    },
+    {
+      time: `04:20`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `S7 22`,
+      status: `Ожидается в 03:57`
+    },
+    {
+      time: `03:09`,
+      destination: `СИМФЕРОПОЛЬ`,
+      id: `U6 34`,
+      status: `Прибыл`
+    },
+    {
+      time: `02:31`,
+      destination: `СИМФЕРОПОЛЬ`,
+      id: `U6 48`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:40`,
+      destination: `КАЗАНЬ`,
+      id: `S7 62`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:45`,
+      destination: `ИРКУТСК`,
+      id: `6R 566`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:50`,
+      destination: `АНТАЛЬЯ`,
+      id: `U6 3002`,
+      status: `Ожидается по расписанию`
+    },
+    {
+      time: `11:50`,
+      destination: `ЯКУТСК`,
+      id: `GH 110`,
+      status: `Ожидается по расписанию`
+    }
+  ],
+  departures: [
+    {
+      time: `02:00`,
+      destination: `ДЖЕРБА`,
+      id: `WZ 9687`,
+      status: `Вылетел`
+    },
+    {
+      time: `02:14`,
+      destination: `НОВЫЙ УРЕНГОЙ`,
+      id: `GH 419`,
+      status: `Вылетел`
+    },
+    {
+      time: `02:59`,
+      destination: `ДАЛАМАН`,
+      id: `S7 625`,
+      status: `Вылетел`
+    },
+    {
+      time: `03:12`,
+      destination: `НОЯБРЬСК`,
+      id: `S7 625`,
+      status: `Вылетел`
+    },
+    {
+      time: `03:15`,
+      destination: `ИЗМИР`,
+      id: `PC 6779`,
+      status: `Вылетел`
+    },
+    {
+      time: `03:23`,
+      destination: `НАДЫМ`,
+      id: `YC 173`,
+      status: `Вылетел`
+    },
+    {
+      time: `03:25`,
+      destination: `НОВОСИБИРСК`,
+      id: `GH 179`,
+      status: `Задержан`
+    },
+    {
+      time: `03:27`,
+      destination: `ПАФОС`,
+      id: `GH 179`,
+      status: `Вылетел`
+    },
+    {
+      time: `03:30`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `U6 79`,
+      status: `Задержан`
+    },
+    {
+      time: `03:28`,
+      destination: `НЕАПОЛЬ`,
+      id: `S7 605`,
+      status: `Регистрация`
+    },
+    {
+      time: `03:30`,
+      destination: `ЛАРНАКА`,
+      id: `GH 603`,
+      status: `Регистрация`
+    },
+    {
+      time: `03:32`,
+      destination: `ОМСК`,
+      id: `IO 202`,
+      status: `Задержан`
+    },
+    {
+      time: `12:15`,
+      destination: `СТАМБУЛ`,
+      id: `PC 389`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `АНАПА`,
+      id: `U6 157`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `ФРАНКФУРТ`,
+      id: `S7 3355`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `БАКУ`,
+      id: `S7 3355`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:25`,
+      destination: `БУРГАС`,
+      id: `S7 923`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:30`,
+      destination: `САМАРА`,
+      id: `TP 8146`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:30`,
+      destination: `АСТРАХАНЬ`,
+      id: `EY 4341`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:40`,
+      destination: `ЕРЕВАН`,
+      id: `GH 905`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:45`,
+      destination: `ДЮССЕЛЬДОРФ`,
+      id: `S7 991`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:45`,
+      destination: `БЕРЛИН`,
+      id: `S7 895`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:50`,
+      destination: `АБУ ДАБИ`,
+      id: `EY 068`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:55`,
+      destination: `САНКТ-ПЕТЕРБУРГ`,
+      id: `S7 39`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:55`,
+      destination: `ДОХА`,
+      id: `QR 234`,
+      status: `По расписанию`
+    },
+    {
+      time: `12:55`,
+      destination: `ДЖЕРБА`,
+      id: `ZF 7711`,
+      status: `По расписанию`
+    }
+  ]
+};
